bb978481a8aec4774cbcb6ee6cb80ab93d0d5029
Raynor <1101732819@qq.com>
2019-05-26 14:06:36 +0800

    5/26

---
+++ demo/src/main/java/com/demo/mapper/TestMapper.java
@@ -0,0 +1,16 @@
+package com.demo.mapper;
+
+import com.demo.entity.Test;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Raynor
+ * @since 2019-05-26
+ */
+public interface TestMapper extends BaseMapper<Test> {
+
+}
