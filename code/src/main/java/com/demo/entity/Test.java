7b56a1d62c57d53fc36274e7fcc7362a78b9f643
Raynor <1101732819@qq.com>
2019-06-08 22:10:23 +0800

    test1
    Signed-off-by: Raynor <1101732819@qq.com>

---
+++ code/src/main/java/com/demo/entity/Test.java
@@ -0,0 +1,32 @@
+package com.demo.entity;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Raynor
+ * @since 2019-06-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id" , type = IdType.AUTO)
+    private long id;
+
+    private String tName;
+
+
+}
---
+++ code/src/main/java/com/demo/mapper/TestMapper.java
@@ -0,0 +1,16 @@
+package com.demo.mapper;
+
+import com.demo.entity.Test;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Raynor
+ * @since 2019-06-08
+ */
+public interface TestMapper extends BaseMapper<Test> {
+
+}
